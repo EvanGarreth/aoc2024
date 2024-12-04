957d4b270fccfd6914d48a58207ffc06c2a2a513
Evan Campbell <evan.garreth@gmail.com>
2024-12-04 16:42:53 -0600

    d4 pt2 & commit Cargo.toml...

--- d4/src/main.rs
+++ d4/src/main.rs
@@ -14,6 +14,12 @@ fn main() -> std::io::Result<()> {
 
     println!("Part 1 result: {p1_result}");
 
+    let p2_contents = BufReader::new(File::open("src/input/data.txt")?);
+
+    let p2_result = part2(p2_contents);
+
+    println!("Part 2 result: {p2_result}");
+
     return Ok(());
 }
 
@@ -106,6 +112,43 @@ fn part1(reader: BufReader<File>) -> i32 {
     return matches;
 }
 
+fn part2(reader: BufReader<File>) -> i32 {
+    let graph: Vec<Vec<char>> = reader
+        .lines()
+        .map(|l| l.unwrap().chars().collect())
+        .collect();
+
+    let rows = graph.len();
+    let cols = graph[0].len();
+
+    let mut matches = 0;
+
+    for (i, row) in graph.iter().enumerate() {
+        for (j, col) in row.iter().enumerate() {
+            if *col != 'M' && *col != 'S' {
+                continue;
+            }
+
+            // valid 3x3 square
+            if i + 2 < rows && j + 2 < cols {
+                let mid = graph[i + 1][j + 1] == 'A';
+
+                let l_diag = (graph[i][j] == 'M' && graph[i + 2][j + 2] == 'S')
+                    || (graph[i][j] == 'S' && graph[i + 2][j + 2] == 'M');
+
+                let r_diag = (graph[i + 2][j] == 'M' && graph[i][j + 2] == 'S')
+                    || (graph[i + 2][j] == 'S' && graph[i][j + 2] == 'M');
+
+                if l_diag && mid && r_diag {
+                    matches = matches + 1;
+                }
+            }
+        }
+    }
+
+    return matches;
+}
+
 #[cfg(test)]
 mod tests {
     use std::io::BufReader;
@@ -121,4 +164,12 @@ mod tests {
         assert_eq!(result, 18);
     }
 
+    #[test]
+    fn p2() {
+        let contents = BufReader::new(File::open("src/input/test.txt").unwrap());
+
+        let result = part2(contents);
+
+        assert_eq!(result, 9);
+    }
 }
